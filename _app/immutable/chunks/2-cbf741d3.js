61bdf820057996096d3b3ddb60ba1aa8c2f653c2
freitzzz <1160907@isep.ipp.pt>
2023-02-12 00:34:03 +0000

    Updates

--- _app/immutable/chunks/2-cbf741d3.js
+++ _app/immutable/chunks/2-cbf741d3.js
@@ -1,5 +1,5 @@
 import { _ } from "./_page-45aecac1.js";
-import { default as default2 } from "../components/pages/_page.svelte-ecf7b655.js";
+import { default as default2 } from "../components/pages/_page.svelte-6766bb26.js";
 export {
   default2 as component,
   _ as universal
